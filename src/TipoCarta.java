ebe6cfc19b13cc0b41ca6d6eba312e11ebbb3d45
JhoanLopez-afk <jhoan.sebastian.lopez@correounivalle.edu.co>
2026-04-02 16:18:22 -0500

    Modifique clases y eliminé la clase Trampa, para que sea mas fiel a las peticiones de la guia

--- src/TipoCarta.java
+++ src/TipoCarta.java
@@ -1,3 +1,3 @@
 public enum TipoCarta {
-    MOUNSTRUO, MAGICA, TRAMPA
+    MOUNSTRUO, MAGICA
 }
--- src/Trampa.java
+++
@@ -1,6 +0,0 @@
-public class Trampa extends Carta {
-    public Trampa(String nombre, TipoCarta tipo, boolean visible, String efecto, Estado estado) {
-        super(nombre, tipo, visible, efecto, estado);
-    }
-    
-}
