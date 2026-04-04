6fe6616000ff4f9859da3cadc3ec0c161adbdedc
JhoanLopez-afk <jhoan.sebastian.lopez@correounivalle.edu.co>
2026-04-03 21:41:27 -0500

    generé un mazo de baraja, con 30 monstruos y 10 magias

--- src/App.java
+++ src/App.java
@@ -1,10 +1,57 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Mounstro carta1 = new Mounstro("Carta1", (short) 3000, (short) 2000, TipoCarta.MOUNSTRUO, Mounstro.Estrellas.Cinco, true, "Efecto de la carta1", Estado.MANO);
-        Magia carta2 = new Magia("Carta2", TipoCarta.MAGICA, false , "Efecto de la carta2", Estado.CEMENTERIO);
 
-        System.out.println(carta1.getNombre() + " - " + carta1.getTipo() + " - " + carta1.isVisible() + " - " + carta1.getEstrellas() + " - " + carta1.getAtaque() + " - " + carta1.getDefensa() + " - " + carta1.getEfecto() + " - " + carta1.getEstado());
-        System.out.println(carta2.getNombre() + " - " + carta2.getTipo() + " - " + carta2.isVisible() + " - " + carta2.getEfecto() + " - " + carta2.getEstado());
+        List<Carta> BarajaDeCartas = generarBaraja();
+
+        
+
+
+
+
+
+
+
 
     }
+
+    public static List<Carta> generarBaraja() {
+    List<Carta> mazoCompleto = new ArrayList<>();
+    String[] nombres = {"Mago Oscuro", "Dragón Blanco","Fantasma Flamigero", "Dragon Negro", "Guardián Celta",
+     "El Mega-Ciber Demniaco","Gaitero Dragon","Dama Arpia", "Craneo Convocado", "Araña Cazadora","Gilasaurus","Dama Pantera",
+     "Caballero Robotico","Guardian Fiable","Demonio Menor","Gran Tiki Anciano", "Arquera Amazoness", "Caiman Toon",
+     "Dragon Craneo Demonio", "Capitan Merodeador", "Ave de Craneo Rojo", "Balter Oscuro","Cazador de Espadas",
+     "Baron de la Espada Demoniaca","Egotista Elegante", "Bebe Dragon","Cebra Oscura","Espia Inexperto", "Behemoth",
+     "Bruja Oscura"};
+    
+    // Crear 30 Monstruos con stats variados
+    for (int i = 0; i < 30; i++) {
+        String nombre = nombres[i];
+        short atk = (short) (1000 + ((int)(Math.random()*181)+200)); // El ataque sube un poco en cada uno
+        short def = (short) (800 + ((int)(Math.random()*21)+50));
+        Mounstro.Estrellas aleatoria = Mounstro.Estrellas.values()[new Random().nextInt(Mounstro.Estrellas.values().length)]; 
+        
+        mazoCompleto.add(new Mounstro(nombre, atk, def, TipoCarta.MOUNSTRUO, 
+                         aleatoria, false, "Sin efecto", Estado.BARAJA));
+    }
+
+    // Crear 10 Magias
+    for (int i = 0; i < 10; i++) {
+        NombreMagia NM = NombreMagia.values()[new Random().nextInt(NombreMagia.values().length)];
+
+
+        mazoCompleto.add(new Magia(NM + "i", TipoCarta.MAGICA, 
+                         false, "Efecto básico", Estado.BARAJA));
+    }
+
+    // ¡IMPORTANTE! Barajar para que sea al azar (Requisito RF2)
+    Collections.shuffle(mazoCompleto);
+    
+    return mazoCompleto;
+}
+
 } 
--- src/Magia.java
+++ src/Magia.java
@@ -1,6 +1,14 @@
-public class Magia extends Carta {
+public class Magia extends Carta implements Activable{
+
     public Magia(String nombre, TipoCarta tipo, boolean visible, String efecto, Estado estado) {
         super(nombre, tipo, visible, efecto, estado);
     }
+
+    @Override
+    public void ejecutarEfecto(Jugador usuario, Jugador oponente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ejecutarEfecto'");
+    }
+    
     
 }
